31b0fc519e8d750f25dee68540493c02ed8de685
Jérémy DECOOL <contact@jdecool.fr>
2021-08-16 21:14:50 +0200

    Fix exception breaking change compatibility

--- src/UnparsableStringException.php
+++ src/UnparsableStringException.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Novutec Domain Tools.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,20 +20,20 @@
  */
 
 /**
- * @namespace Novutec\DomainParser\Exception
+ * @namespace Novutec\DomainParser
  */
-namespace Novutec\DomainParser\Exception;
-
-use Novutec\DomainParser\OpenFileException as BaseOpenFileException;
+namespace Novutec\DomainParser;
 
 /**
- * OpenFileException.
+ * UnparsableStringException.
  *
  * @category   Novutec
  *
  * @copyright  Copyright (c) 2007 - 2013 Novutec Inc. (http://www.novutec.com)
  * @license    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @deprecated use Novutec\DomainParser\Exception\UnparsableStringException instead
  */
-class OpenFileException extends BaseOpenFileException
+class UnparsableStringException extends AbstractException
 {
 }
